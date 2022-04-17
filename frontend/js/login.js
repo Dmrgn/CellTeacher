0d3f46032f1adc2c9eb9f18ac350156d7ec086b3
katskreativity <cmakarytchev@gmail.com>
2022-04-17 01:55:36 -0400

    time to sleep now T-T

---
+++ frontend/js/login.js
@@ -0,0 +1,41 @@
+// add login event listener
+const butElm = document.querySelector("#submit");
+const userElm = document.querySelector("#username");
+const passElm = document.querySelector("#password");
+const capElm = document.querySelector("#captcha");
+const compElm = document.querySelector("#complainer");
+const captchaElm = document.querySelector("#captchaImg");
+
+// id of the generated captcha
+let captchaId = null;
+
+// get a captcha when the page loads
+// getCaptcha was moved to utils.js
+getCaptcha().then(data => {
+    data.json().then(json => {
+        captchaElm.innerHTML = json.captcha;
+        captchaId = json.captchaId;
+    })
+});
+
+butElm.addEventListener("click", async (e) => {
+    const loginAttempt = await login(userElm.value, passElm.value);
+    if ((loginAttempt.status+'').charAt(0) == '2')
+        return window.location = "https://cellteacher.herokuapp.com/users/index";
+    if (loginAttempt.status === 422)
+        return window.location = window.location.origin + "/error?message=Captcha failed, please enter the correct Captcha Code.";
+    compElm.innerText = await loginAttempt.text();
+})
+
+// login with the current credentials in the input
+async function login(name, password) {
+    // send login request
+    return sendRequest("https://cellteacher.herokuapp.com/users/login", "POST", {
+        name: name,
+        password: password,
+        captcha: {
+            id: captchaId,
+            text: capElm.value
+        }
+    })
+}
