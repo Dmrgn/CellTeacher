805d0cfab89e4864d3564e03c0ae12ccf940d78c
Dmrgn <danielrmorgan11@gmail.com>
2022-04-17 12:15:25 -0400

    thing

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -11,62 +11,12 @@ let generatorFacing, generatorType;
 let duplicatorIn, duplicatorOut;
 let dirUp, dirLeft, dirDown, dirRight;
 
-var levels = [
-    [
-        7, // board size
-        100, // cell size
-        [0, 1, 2, 6], // buildArea
-        [
-            [1, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 1],
-            [1, 0, 6, 6, 6, 0, 1],
-            [1, 1, 1, 1, 1, 1, 1]
-        ]
-    ],
-    [
-        7, // board size
-        100, // cell size
-        [0, 0, 2, 2], // buildArea
-        [
-            [0, 0, 0, 0, 0, 0, 1],
-            [0, 0, 0, 0, 0, 0, 1],
-            [0, 0, 0, 0, 0, 0, 1],
-            [0, 0, 0, 0, 0, 0, 1],
-            [0, 0, 0, 0, 6, 6, 1],
-            [0, 0, 0, 0, 6, 6, 1],
-            [1, 1, 1, 1, 1, 1, 1]
-        ]
-    ],
-    [
-        15, // board size
-        50, // cell size
-        [0, 0, 15, 15], // buildArea
-        [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        ]
-    ],
-]
-
 function setup() {
     frameRate(30);
+
+    boardSize = 60;
+    cellSize = 15;
+    sideBarSize = 60;
 
     initCamera();
 
@@ -76,26 +26,15 @@ function setup() {
     createCanvas(htmlElm.clientWidth/3*2, htmlElm.clientHeight);
     canvas.style = "display: flex;";
     drawingContext.imageSmoothingEnabled = false;
-}
-
-function levelSetup(lev) {
-    // levelData = await sendRequest(("https://cellteacher.herokuapp.com/levels/" + String(lev)), "GET");
-
-    boardSize = levelData.boardSize;
-    cellSize = Math.round(Math.min(htmlElm.clientHeight, htmlElm.clientWidth)/boardSize);
-    sideBarSize = 200;
-    sideBarHeight = 60;
-    buildArea = levelData.buildArea;
 
     moveCamera(width/2-200-cellSize*boardSize/2, height/2-cellSize*boardSize/2);
 
-    types = ["Blank", "Wall", "Moveable", "Pusher", "Generator", "Duplicator", "Goal"];
-
-    // board = Array(boardSize);
-    // for (let x = 0; x < boardSize; x++)
-    //     board[x] = levels[lev][3].slice();
+    board = Array(boardSize);
+    for (let x = 0; x < boardSize; x++)
+        board[x] = Array(boardSize).fill(0);
 
     cols = [color(223), color(0), color(165, 114, 63), color(255, 255, 0), color(0, 255, 255), color(255, 0, 255), color(0, 255, 0)];
+    types = ["Blank", "Wall", "Moveable", "Pusher", "Generator", "Duplicator", "Goal"];
     xOffset = 200;
     yOffset = 0;
     currCell = 1;
@@ -123,9 +62,6 @@ function keyTyped() {
 
 function draw() {
     background("white");
-
-    scriptElm.value = scripts[currCell] ?? "" ;
-    console.log(scripts[currCell]);
     
     startCamera();
 
@@ -149,24 +85,6 @@ function draw() {
     if (play && frameCount%6 === 0) {
         board = step(board);
     }
-    if (levelComplete(board) && currLevel < levels.length) {
-        levelSetup(++currLevel);
-    }
-    noFill();
-    stroke(127);
-    strokeWeight(10);
-    rect(sideBarSize + buildArea[0]*cellSize, buildArea[1]*cellSize, (buildArea[2]-buildArea[0])*cellSize, (buildArea[3]-buildArea[1])*cellSize);
-    stroke(0);
-    strokeWeight(2);
-}
-
-function levelComplete(board) {
-    for (let x = 0; x < board.length; x++) {
-        for (let y = 0; y < board[x].length; y++) {
-            if (board[x][y] == 6) return false;
-        }
-    }
-    return true;
 }
 
 function drawBoard(board) {
@@ -178,7 +96,7 @@ function drawBoard(board) {
     }
 }
 
-function moveCell(board, arr, x, y, dir) {
+function move(board, arr, x, y, dir) {
     if (x + dir[0] == 0 || x + dir[0] == 6
      || y + dir[0] == 0 || y + dir[0] == 6)
         arr[x + dir[0]][y + dir[1]] = board[x][y];
@@ -195,9 +113,11 @@ function step(board) {
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board.length; j++) {
             if (board[i][j] == 1) {
+                console.log(board[i][j]);
                 try {
                     eval(scripts[board[i][j]]);
                 } catch (error) {}
+                console.log(board[i][j]);
             }
         }
     }
@@ -247,13 +167,13 @@ function step(board) {
                 for (let i = 0; i < move - adj; i++) {
                     if (x + (i + 1)*pusherFacing[0] >= boardSize 
                     || y + (i + 1)*pusherFacing[1] >= boardSize) break;
-                    board, arr = moveCell(board, arr, x, y, pusherFacing);
+                    board, arr = move(board, arr, x, y, pusherFacing);
                     // arr[x + (i + 1)*pusherFacing[0]][y + (i + 1)*pusherFacing[1]] = board[x + i*pusherFacing[0]][y + i*pusherFacing[1]];
                     if (x + (i - 1)*pusherFacing[0] < 0
                      || x + (i + 1)*pusherFacing[0] >= boardSize
                      || y + (i - 1)*pusherFacing[1] < 0
                      || y + (i + 1)*pusherFacing[1] >= boardSize)
-                        board, arr = moveCell(board, arr, x - pusherFacing[0], y - pusherFacing[1], [-pusherFacing[0], -pusherFacing[1]]);
+                        board, arr = move(board, arr, x - pusherFacing[0], y - pusherFacing[1], [-pusherFacing[0], -pusherFacing[1]]);
                     // arr[x + i*pusherFacing[0]][y + i*pusherFacing[1]] = x + (i - 1)*pusherFacing[0] < 0 || x + (i + 1)*pusherFacing[0] >= boardSize || y + (i - 1)*pusherFacing[1] < 0 || y + (i + 1)*pusherFacing[1] >= boardSize ? 0 : board[x + (i - 1)*pusherFacing[0]][y + (i - 1)*pusherFacing[1]];
                 }
                 if (move - adj > 0) arr[x][y] = 0;
@@ -304,14 +224,17 @@ function mouseDragged() {
         return;
 
     dragged = true;
+
     const mouseTileX = Math.floor((mouseX - cameraPos.x)/zoom);
     const mouseTileY = Math.floor((mouseY - cameraPos.y)/zoom);
     if (mouseButton === LEFT) {
-        if (mouseX < sideBarSize) {
-            if (mouseY < sideBarHeight * cols.length) {
-                currCell = int(mouseY / sideBarHeight);
+        if (mouseX < 200) {
+            if (mouseY < sideBarSize * cols.length) {
+                currCell = int(mouseY / sideBarSize);
+                print(currCell);
+                updateUI();
             }
-        } else if (mouseTileX >= Math.floor(sideBarSize/boardSize) && mouseTileX <= boardSize*cellSize + sideBarSize && mouseTileY <= boardSize*cellSize) {
+        } else if (mouseTileX >= 200 && mouseTileX <= boardSize*cellSize + 200 && mouseTileY <= boardSize*cellSize) {
             board[int((mouseTileX - xOffset) / cellSize)][int((mouseTileY - yOffset) / cellSize)] = currCell;
         }
     }
